363e7fa1c1da1551ba3860fc9f1da811a8b22f32
NxPKG <116948796+NxPKG@users.noreply.github.com>
2023-11-14 01:22:16 +0600

    Create main.go
    Signed-off-by: NxPKG <116948796+NxPKG@users.noreply.github.com>

---
+++ main.go
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Khulnasoft
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/khulnasoft/certgen/cmd"
+)
+
+func main() {
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
